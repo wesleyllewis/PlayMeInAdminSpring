40642c7c038500d48362fa0a9471d63acdf26310
Wesley Lewis <WesleyLewis@wesleys-MacBook-Air.local>
2016-11-11 09:53:34 -0600

    initial, user.class is done

---
+++ src/main/java/com/theironyard/entities/User.java
@@ -0,0 +1,45 @@
+package com.theironyard.entities;
+
+
+import com.theironyard.utilities.PasswordStorage;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {}
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getPasswordHash() {return password;}
+
+    public void setPasswordHash(String password) throws PasswordStorage.CannotPerformOperationException{
+        this.password = PasswordStorage.createHash(password);
+    }
+
+    public boolean isValid(String password) throws Exception{
+        return PasswordStorage.verifyPassword(password, getPasswordHash());
+    }
+}
